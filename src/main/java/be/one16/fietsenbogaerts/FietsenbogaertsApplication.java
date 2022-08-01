2af66ddc718f078b1b4b6546fb8162b505efe0da
Arno Maddelein <arno.maddelein@one16.be>
2022-08-01 11:03:24 +0200

    Add Spring initializer files

---
+++ src/main/java/be/one16/fietsenbogaerts/FietsenbogaertsApplication.java
@@ -0,0 +1,13 @@
+package be.one16.fietsenbogaerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietsenbogaertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FietsenbogaertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/one16/fietsenbogaerts/FietsenbogaertsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.one16.fietsenbogaerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietsenbogaertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
