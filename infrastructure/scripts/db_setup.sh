d55ed214347ba5335fb52da546d8ee0030dc6458
Kevin Smeyers <kevin.smeyers@one16.be>
2022-08-01 16:22:29 +0200

    nieuwe docker compose

--- infrastructure/scripts/db_setup.sh
+++
@@ -1,4 +0,0 @@
-/opt/mssql-tools//bin/sqlcmd -S mssqlserver -U sa -P Th1s1sMyPass_word -Q  'CREATE DATABASE barka_db;'
-/opt/mssql-tools//bin/sqlcmd -S mssqlserver -U sa -P Th1s1sMyPass_word -d barka_db -Q  'CREATE SCHEMA magazijn;'
-/opt/mssql-tools//bin/sqlcmd -S mssqlserver -U sa -P Th1s1sMyPass_word -d barka_db -Q  'CREATE SCHEMA klant;'
-/opt/mssql-tools//bin/sqlcmd -S mssqlserver -U sa -P Th1s1sMyPass_word -d barka_db -Q  'CREATE SCHEMA leverancier;'
