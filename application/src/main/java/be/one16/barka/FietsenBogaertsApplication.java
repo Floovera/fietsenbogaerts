fcd4960786df10bd1094a585dec0ebc7d7bd7417
kwintenderidder <kwinten.deridder@one16.be>
2022-08-05 13:38:48 +0200

    Delete application package and move Application class

---
+++ application/src/main/java/be/one16/barka/FietsenBogaertsApplication.java
@@ -0,0 +1,11 @@
+package be.one16.barka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietsenBogaertsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FietsenBogaertsApplication.class, args);
+    }
+}
